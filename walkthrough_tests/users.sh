bbdbfcce76d399493349ca92a180131679aa2a7e
Timothy Corbett-Clark <timothy.corbettclark@gmail.com>
2020-03-10 14:44:50 +0000

    Added more tests for users subcommand

--- walkthrough_tests/users.sh
+++ walkthrough_tests/users.sh
@@ -8,14 +8,29 @@ source $(dirname "$0")/lib.sh
 
 start_tests
 
-test "List all users"
-encapsia users list --all-users
+test "Add superuser"
+encapsia users add-superuser tcorbettclark@cmedtechnology.com Timothy Corbett-Clark
 
 test "List all superusers"
 encapsia users list --super-users
 
+test "Delete superuser"
+encapsia users delete tcorbettclark@cmedtechnology.com
+
+
+test "Add system user"
+encapsia users add-systemuser a-description cap1,cap2,cap3
+
 test "List all system users"
 encapsia users list --system-users
+
+test "Delete system user"
+encapsia users delete system@a-description.encapsia.com
+
+
+test "List all users"
+encapsia users list --all-users
+
 
 test "Export all users and roles"
 encapsia users export /tmp/test_encapsia_users.toml
