2f856777565a5ad17dc1539f6b8d8da71013bff8
Timothy Corbett-Clark <timothy.corbettclark@gmail.com>
2020-05-13 20:33:45 +0000

    Comment out the "use fixture" test as too dangerous!

--- walkthrough_tests/fixtures.sh
+++ walkthrough_tests/fixtures.sh
@@ -11,9 +11,10 @@ start_tests
 test "Create a fixture"
 encapsia fixtures create test_fixture
 
-test "Use a fixture"
-encapsia fixtures use --yes test_fixture
-sleep 5
+# Don't test using a fixture because too disruptive of target server
+test "Use a fixture (DISABLED: to be less invasive on test server)"
+# encapsia fixtures use --yes test_fixture
+# sleep 5
 
 test "Delete a fixture"
 encapsia fixtures delete --yes test_fixture
