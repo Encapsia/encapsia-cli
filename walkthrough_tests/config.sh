8923bdf6ddf3f7a34143e6edb78f63cdaab680fb
Timothy Corbett-Clark <timothy.corbettclark@gmail.com>
2020-09-04 20:56:22 +0100

    Updated output from tests

--- walkthrough_tests/config.sh
+++ walkthrough_tests/config.sh
@@ -24,7 +24,7 @@ encapsia config delete test_list
 
 test "Save config to a file and load it again"
 encapsia config save /tmp/test_encapsia_config.json
-cat /tmp/test_encapsia_config.json
+tail /tmp/test_encapsia_config.json
 
 # Remove the root_token because it is hidden and we cannot set it again.
 cat /tmp/test_encapsia_config.json | python -c "import json, sys; c=json.load(sys.stdin); del c['root_token']; print(json.dumps(c))" > /tmp/test_encapsia_config2.json
@@ -34,4 +34,4 @@ encapsia config load /tmp/test_encapsia_config2.json
 rm /tmp/test_encapsia_config*.json
 
 test "Show the entire config"
-encapsia config show
+encapsia config show | tail
