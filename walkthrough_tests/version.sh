5fa96a48bb6ba43ac7273e95d6695429f278b3a6
Timothy Corbett-Clark <timothy.corbettclark@gmail.com>
2020-09-15 10:55:40 +0100

    Refactor

--- walkthrough_tests/version.sh
+++ walkthrough_tests/version.sh
@@ -1,18 +1,9 @@
 #/usr/bin/env bash
 
-# Pretty print the test descriptions.
-function test() {
-    echo -e "\n\e[93m\e[1m=== $1 ===\e[0m"
-}
+# Load "library" code
+source $(dirname "$0")/lib.sh
 
-# Always fail on error.
-set -e
-
-# Log commands except for echo because they are used to explain what is being done.
-trap '[[ $BASH_COMMAND != test* ]] && echo -e ">${BASH_COMMAND}"' DEBUG
-
-
-# WALKTHROUGH STARTS HERE...
+start_tests
 
 test "Print out the version"
 encapsia version
