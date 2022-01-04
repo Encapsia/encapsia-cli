52c2aded06f6c98e2960f95912ef9fb38ccf2bff
Petre Mierlutiu <pmierlutiu@cmedtechnology.com>
2022-01-04 15:45:07 +0200

    Add and extend token subcommands.
    * Add `token env` showing shell commands to set encapsia environment variables.
    * Add `token transfer` to obtain a token for a different user.
    * Change `token extend` to be able to display token instead of storing (plain text or as shell commands).
    * Change `token extend` to be able to set capabilities.
    * Moved isort configuration to pyproject.toml.

--- encapsia_cli/token.py
+++ encapsia_cli/token.py
@@ -1,7 +1,11 @@
 import click
-from encapsia_api import CredentialsStore, EncapsiaApiError
+from encapsia_api import CredentialsStore, EncapsiaApi, EncapsiaApiError
 
 from encapsia_cli import lib
+
+
+# By default, create tokens with one year lifespan when extending, transfering.
+DEFAULT_LIFESPAN = 60 * 60 * 24 * 365
 
 
 @click.group("token")
@@ -37,19 +41,102 @@ def whoami(obj):
 @main.command()
 @click.option(
     "--lifespan",
-    default=60 * 60 * 24 * 365,
+    default=DEFAULT_LIFESPAN,
     help="New lifespan of token. Default is 1 year.",
 )
+@click.option(
+    "--capabilities",
+    default=None,
+    help="New capabilities of token. Default is to keep the same.",
+)
+@click.option(
+    "--store/--no-store",
+    default=True,
+    is_flag=True,
+    help="Replace new token in credentials file.",
+)
+@click.option(
+    "--plain",
+    "display",
+    flag_value="plain",
+    default=True,
+)
+@click.option(
+    "--env",
+    "display",
+    flag_value="shell",
+)
+@click.option(
+    "--shell",
+    type=click.Choice(["auto", "bash", "fish", "zsh"], case_sensitive=False),
+    default="auto",
+    help="Target shell. Default is to autodetect.",
+)
 @click.pass_obj
-def extend(obj, lifespan):
+def extend(obj, lifespan, capabilities, store, display, shell):
     """Extend the lifespan of token and update encapsia credentials (if used)."""
     api = lib.get_api(**obj)
-    new_token = api.login_again(lifespan=lifespan)
+    if capabilities is not None:
+        capabilities = [c.strip() for c in capabilities.split(",")]
+    new_token = api.login_again(lifespan=lifespan, capabilities=capabilities)
     host = obj.get("host")
-    if host:
+    if host and store:
         store = CredentialsStore()
-        url, old_token = store.get(host)
+        url, _ = store.get(host)
         store.set(host, url, new_token)
         lib.log("Encapsia credentials file updated.")
     else:
-        lib.log(new_token)
+        lib.print_token(new_token, display, url=api.url, shell=shell)
+
+
+@main.command()
+@click.option(
+    "--lifespan",
+    default=DEFAULT_LIFESPAN,
+    help="New lifespan of token. Default is 1 year.",
+)
+@click.option(
+    "--plain",
+    "display",
+    flag_value="plain",
+    default=True,
+)
+@click.option(
+    "--env",
+    "display",
+    flag_value="shell",
+)
+@click.option(
+    "--shell",
+    type=click.Choice(["auto", "bash", "fish", "zsh"], case_sensitive=False),
+    default="auto",
+    help="Target shell. Default is to autodetect.",
+)
+@click.argument(
+    "user",
+)
+@click.pass_obj
+def transfer(obj, lifespan, display, shell, user):
+    """Get a token for `user` (subject to proper capabilities)"""
+    api = lib.get_api(**obj)
+    user_token = api.login_transfer(user, lifespan=lifespan)
+    # login_transfer does not use the lifespan argument;
+    # see https://github.com/tcorbettclark/encapsia-api/issues/32
+    user_api = EncapsiaApi(api.url, user_token)
+    extended_user_token = user_api.login_again(lifespan=lifespan)
+    lib.print_token(extended_user_token, display, url=api.url, shell=shell)
+
+
+@main.command()
+@click.option(
+    "--shell",
+    type=click.Choice(["auto", "bash", "fish", "zsh"], case_sensitive=False),
+    default="auto",
+    help="Target shell. Default is to autodetect.",
+)
+@click.pass_obj
+def env(obj, shell):
+    """Generate shell commands to populate encapsia host environment variables."""
+    host = obj.get("host")
+    url, token = CredentialsStore().get(host)
+    lib.print_token(token, "shell", url, shell)
--- walkthrough_tests/token.sh
+++ walkthrough_tests/token.sh
@@ -8,8 +8,16 @@ start_tests
 test "Get whoami info about current token"
 encapsia token whoami
 
+test "Display shell commands to set ENCAPSIA_URL and ENCAPSIA_TOKEN environment variables for host"
+encapsia token env
+
 test "Extend lifespan of token (changes the encapsia credentials)"
 encapsia token extend
+encapsia token whoami
+
+test "Transfer (get new token) to specified user. Stored token *should not* change!"
+encapsia users add-superuser user1@cmedtechnology.com User One
+encapsia token transfer user1@cmedtechnology.com
 encapsia token whoami
 
 # Don't normally run the following because it is too distructive!
