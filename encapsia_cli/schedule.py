e096574d6ec11f34ab1bebdc78147218a3a5b1f4
Ross Elliott <relliott@cmedtechnology.com>
2022-12-23 12:44:45 +0000

    additional schedule commands

--- encapsia_cli/schedule.py
+++ encapsia_cli/schedule.py
@@ -1,4 +1,7 @@
+import json
+
 import click
+import toml
 
 from encapsia_cli import lib
 
@@ -19,6 +22,7 @@ def list_tasks(obj):
 
 
 @main.command("add")
+@click.option("--name", prompt="Name", required=False)
 @click.option("--description", prompt="Description", required=True)
 @click.option("--task-host", prompt="Task host", required=True)
 @click.option("--task-token", prompt="Task token", required=True)
@@ -33,7 +37,7 @@ def list_tasks(obj):
 @click.option("--jitter", prompt="Jitter (int)", type=int, required=True)
 @click.pass_obj
 def add_task(
-    obj, description, task_host, task_token, namespace, task, params, cron, jitter
+    obj, name, description, task_host, task_token, namespace, task, params, cron, jitter
 ):
     """Add new scheduled task."""
     api = lib.get_api(**obj)
@@ -41,6 +45,7 @@ def add_task(
         api,
         "add_scheduled_task",
         dict(
+            name=name,
             description=description,
             host=task_host,
             token=task_token,
@@ -80,3 +85,75 @@ def remove_task(obj, scheduled_task_id):
         dict(scheduled_task_id=scheduled_task_id),
         "Removing scheduled tasks",
     )
+
+
+@main.command()
+@click.argument("namespace")
+@click.argument("name")
+@click.pass_obj
+def remove_task_by_name(obj, namespace, name):
+    """Remove scheduled task by namespace and name."""
+    api = lib.get_api(**obj)
+    schedules = lib.run_plugins_task(
+        api, "list_scheduled_tasks", {}, "Fetching list of scheduled tasks"
+    )
+    for schedule in json.loads(schedules):
+        if (schedule['namespace'] == namespace):
+            if schedule['name'] == name:
+                id = schedule['id']
+                lib.run_plugins_task(
+                    api,
+                    "remove_scheduled_task",
+                    dict(scheduled_task_id=id),
+                    f"Removing scheduled task {schedule['name']} with {id}",
+                )
+
+
+@main.command()
+@click.argument("namespace")
+@click.argument("schedule-file")
+def update_schedule(obj, namespace, schedule_file):
+    """Update schedules for existing tasks."""
+    api = lib.get_api(**obj)
+    with open(schedule_file) as f:
+        _config = toml.load(f)
+        replacement_schedules = _config['schedules']
+    new_schedules = {
+        s['name']: s for s in replacement_schedules
+    }
+    schedules = lib.run_plugins_task(
+        api, "list_scheduled_tasks", {}, "Fetching tasks"
+    )
+    for schedule in json.loads(schedules):
+        if (schedule['namespace'] == namespace):
+            if schedule['name'] in new_schedules.keys():
+                id = schedule['id']
+                lib.run_plugins_task(
+                    api,
+                    "remove_scheduled_task",
+                    dict(scheduled_task_id=id),
+                    f"Removing {schedule['name']} with {id} from {namespace}",
+                )
+                new_schedule = new_schedules[schedule['name']]
+                new_fields = {
+                    k: v for k, v in new_schedule.items() if k in (
+                        'description', 'params', 'cron'
+                    )
+                }
+                schedule.update(new_fields)
+                lib.run_plugins_task(
+                    api,
+                    "add_scheduled_task",
+                    dict(
+                        name=schedule['name'],
+                        description=schedule['description'],
+                        host=schedule['host'],
+                        token=schedule['token'],
+                        namespace=schedule['namespace'],
+                        task=schedule['task'],
+                        params=json.dumps(schedule['params']),
+                        cron=schedule['cron'],
+                        jitter=schedule['jitter'],
+                    ),
+                    f"Adding {schedule['name']}",
+                )
